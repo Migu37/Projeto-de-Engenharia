4fa8e8427988cd0881e19ec32e0826b534a6841a
20131tadst0325 <20131tadst0325@ST-LINF3-16.cefetes.br>
2015-05-20 20:27:18 -0300

    sdg

--- Teste/src/Oi.java
+++ Teste/src/Oi.java
@@ -5,6 +5,12 @@ public class Oi {
 		System.out.println("jfj");
 		//kjxncmlbnçxklcjnbçkjlzxcvbz
 		//kjxncmlbnçxklcjnbçkjlzxcvbz
+		//kjxncmlbnçxklcjnbçkjlzxcvbz
+				//kjxncmlbnçxklcjnbçkjlzxcvbz
+		//kjxncmlbnçxklcjnbçkjlzxcvbz
+				//kjxncmlbnçxklcjnbçkjlzxcvbz
+		//kjxncmlbnçxklcjnbçkjlzxcvbz
+				//kjxncmlbnçxklcjnbçkjlzxcvbz
 		
 
 	}
