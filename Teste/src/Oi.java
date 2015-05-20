485e785af3b4259d684b2d8891d0992b9d16c905
20131tadst0325 <20131tadst0325@ST-LINF3-16.cefetes.br>
2015-05-20 19:44:36 -0300

    lh

--- Teste/src/Oi.java
+++ Teste/src/Oi.java
@@ -4,11 +4,10 @@ public class Oi {
 	public static void main(String[] args) {
 	System.out.println("jfj");
 	
-	System.out.println("jfj");
-	System.out.println("jfj");
-
 	
 
+	
+	System.out.println("jfj");System.out.println("jfj");
 
 	}
 
