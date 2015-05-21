231b610417d80090de7bbb6b05ac7f88c49913b4
Migu37 <miguel.wolf22@gmail.com>
2015-05-21 01:55:01 -0300

    Tela inicial Pronta

--- Teste/src/Oi.java
+++ Teste/src/Oi.java
@@ -2,6 +2,9 @@
 
 import Principal.TelaInicial;
 public class Oi {
+import SEBA.telaCadastroFamilia;
+import SEBA.telaCadastroMembro;
+
 	import java.awt.*;
 
 import javax.swing.JFrame;
@@ -42,6 +45,91 @@ import javax.swing.ImageIcon;
 		});
 		
 
+	}
+	public TelaInicial() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1177, 625);
+		
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(51, 204, 51));
+	
+		contentPane.setBackground(UIManager.getColor("CheckBox.background"));
+		contentPane.setToolTipText("");
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Par\u00F3quia Evang\u00E9lica de Confiss\u00E3o Luterana em S\u00E3o Sebasti\u00E3o\r\n\r\n");
+		lblNewLabel.setFont(new Font("Arial Black", Font.ITALIC, 17));
+		lblNewLabel.setBounds(311, 47, 585, 28);
+		contentPane.add(lblNewLabel);
+	
+		
+	
+		JLabel lblCadastros = new JLabel("MEMBROS");
+		lblCadastros.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		lblCadastros.setBounds(460, 189, 131, 14);
+		contentPane.add(lblCadastros);
+		
+		JButton btnMembros = new JButton("");
+		btnMembros.setIcon(new ImageIcon(TelaInicial.class.getResource("/Fotos/membro.jpg")));
+		btnMembros.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				telaCadastroMembro t;
+				try {
+					UIManager.setLookAndFeel(new NimbusLookAndFeel());
+					t = new telaCadastroMembro();
+					t.setLocationRelativeTo(null);
+					t.setVisible(true);
+				} catch (ParseException e) {
+					
+					e.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e) {
+					
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		btnMembros.setBounds(448, 252, 131, 120);
+		contentPane.add(btnMembros);
+		
+		JButton btnFamilias = new JButton("");
+		btnFamilias.setIcon(new ImageIcon(TelaInicial.class.getResource("/Fotos/Familia.jpg")));
+		btnFamilias.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new telaCadastroFamilia().setVisible(true);
+				} catch (ParseException e1) {
+					
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnFamilias.setBounds(610, 252, 131, 120);
+		contentPane.add(btnFamilias);
+		
+		JButton btnSair = new JButton("SAIR");
+		btnSair.setBackground(new Color(250, 128, 114));
+		btnSair.setForeground(new Color(0, 0, 0));
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				System.exit(0);
+			}
+
+			
+		});
+		btnSair.setBounds(883, 519, 127, 42);
+		contentPane.add(btnSair);
+		
+		JLabel lblFamlias = new JLabel("FAM\u00CDLIAS");
+		lblFamlias.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		lblFamlias.setBounds(625, 189, 131, 14);
+		contentPane.add(lblFamlias);
+		
+		
+	
 	}
 
 }
