8f6245aa5f939c2fcf656531ebd24ca52c580932
Migu37 <miguel.wolf22@gmail.com>
2015-05-21 01:41:56 -0300

    87

--- Teste/src/Oi.java
+++ Teste/src/Oi.java
@@ -1,5 +1,21 @@
 
 public class Oi {
+	import java.awt.*;
+	import javax.swing.JFrame;
+	import javax.swing.JPanel;
+	import javax.swing.border.EmptyBorder;
+	import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+	import java.awt.Font;
+	import java.awt.Color;
+	import javax.swing.JLabel;
+	import javax.swing.UIManager;
+	import javax.swing.JButton;
+	import javax.swing.UnsupportedLookAndFeelException;
+	import java.awt.event.ActionListener;
+	import java.awt.event.ActionEvent;
+	import java.text.ParseException;
+	import javax.swing.ImageIcon;
+	2
 
 	public static void main(String[] args) {
 		System.out.println("jfj");
