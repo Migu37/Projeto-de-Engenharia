e8841df5d43f4ede65fa638354bf4a8a8186817c
Migu37 <Migu37@users.noreply.github.com>
2015-05-21 01:32:49 -0300

    Update Oi.java

--- Teste/src/Oi.java
+++ Teste/src/Oi.java
@@ -3,14 +3,7 @@ public class Oi {
 
 	public static void main(String[] args) {
 		System.out.println("jfj");
-		//kjxncmlbnçxklcjnbçkjlzxcvbz
-		//kjxncmlbnçxklcjnbçkjlzxcvbz
-		//kjxncmlbnçxklcjnbçkjlzxcvbz
-				//kjxncmlbnçxklcjnbçkjlzxcvbz
-		//kjxncmlbnçxklcjnbçkjlzxcvbz
-				//kjxncmlbnçxklcjnbçkjlzxcvbz
-		//kjxncmlbnçxklcjnbçkjlzxcvbz
-				//kjxncmlbnçxklcjnbçkjlzxcvbz
+		/
 		
 
 	}
